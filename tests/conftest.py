91ca97c6cdaefd3afa9d30dfd6966768de293de1
Rolnik Boris <rollnicker@mail.ru>
2024-01-06 23:54:24 +0300

    изменен gitignore изменен conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,7 +12,7 @@ def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
-        "browserVersion": "100.0",
+        "browserVersion": "99.0",
         "selenoid:options": {
             "enableVNC": True,
             "enableVideo": True
