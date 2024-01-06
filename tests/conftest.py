5c46eaf887a965dc91dce1e809c481fb3b0b509a
Rolnik Boris <rollnicker@mail.ru>
2024-01-07 01:11:04 +0300

    изменен conftest - взят из selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -21,7 +21,7 @@ def setup_browser(request):
 
     options.capabilities.update(selenoid_capabilities)
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://user1:1234@selenoid:4444/wd/hub",
         options=options
     )
 
