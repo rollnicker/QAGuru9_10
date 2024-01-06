4ea84694389cf0c890992b30e0555e8fa9122f9f
Rolnik Boris <rollnicker@mail.ru>
2024-01-07 00:51:40 +0300

    изменен requirements

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from utils import attach
 
 
 @pytest.fixture(scope='function')
-def setup_browser():
+def setup_browser(request):
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
