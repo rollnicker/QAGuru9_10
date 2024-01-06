856c5f94395764654b733af0ab68910beec6662e
Rolnik Boris <rollnicker@mail.ru>
2024-01-06 23:02:42 +0300

    для jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,18 +1,37 @@
 import pytest
 from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_open():
-    browser.config.base_url = "https://demoqa.com"
-    driver_options = webdriver.ChromeOptions()
-    driver_options.add_argument('--headless')
-    browser.config.driver_options = driver_options
-    browser.config.window_height = 1920
-    browser.config.window_width = 1080
-    browser.config.timeout = 8.0
+def browser_open(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
 
-    yield
+    browser = Browser(Config(driver))
+    browser.config.base_url = "https://demoqa.com"
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
 
     browser.quit()
--- tests/test_automation_practice_form.py
+++ tests/test_automation_practice_form.py
@@ -1,10 +1,16 @@
+import allure
+
 from demoqa_tests.data import users
 from demoqa_tests.steps.automation_practice_form_steps import StudentRegistrationSteps
 
 
-def test_demoqa_steps():
+@allure.title("Successful fill form")
+def test_fill_form_demoqa():
     registration_page = StudentRegistrationSteps()
 
-    registration_page.open()
-    registration_page.register(users.man)
-    registration_page.should_have_registered(users.man)
+    with allure.step("Open registrations form"):
+        registration_page.open()
+    with allure.step("Fill form"):
+        registration_page.register(users.man)
+    with allure.step("Check form results"):
+        registration_page.should_have_registered(users.man)
