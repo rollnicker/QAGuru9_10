5ac0b71075ee656664dffc05116b6bb050c5b6f4
Rolnik Boris <rollnicker@mail.ru>
2024-01-08 13:45:23 +0300

    Добавлена авторизация через dotenv

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+import os
+
 import pytest
 
 from selenium import webdriver
@@ -39,8 +41,11 @@ def setup_browser(request):
 
     options.capabilities.update(selenoid_capabilities)
 
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+
     driver = webdriver.Remote(
-        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
 
